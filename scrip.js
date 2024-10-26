696d203ce2fc06ca2673fb465d16b24df2cff72a
Barbara <araujo.doriana1812@gmail.com>
2024-10-26 11:27:35 -0400

    lo logramos se subio todo

--- scrip.js
+++ scrip.js
@@ -1,1 +1,13 @@
 console.log("hola mundo")
+for (let numero = 0; numero <= 20; numero++) {
+    
+if ( numero /2 === 0 ) {
+ console.log( numero,"este numero es par")
+}
+if (numero /2 !== 0) {
+    console.log( numero,"este numero es impar")
+
+}
+
+
+}
