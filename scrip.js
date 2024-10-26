40b1415cdac9c1fdb13626d666c631d9a01727ae
Barbara <araujo.doriana1812@gmail.com>
2024-10-26 11:51:46 -0400

    bonton

--- scrip.js
+++ scrip.js
@@ -1,13 +1,33 @@
-console.log("hola mundo")
-for (let numero = 0; numero <= 20; numero++) {
-    
-if ( numero /2 === 0 ) {
- console.log( numero,"este numero es par")
+function mifunction() {
+    console.log("hola mundo")
+    for (let numero = 0; numero <= 20; numero++) {
+
+        if (numero / 2 === 0) {
+            console.log(numero, "este numero es par")
+        }
+        if (numero / 2 !== 0) {
+            console.log(numero, "este numero es impar")
+
+        }
+
+
+    }
 }
-if (numero /2 !== 0) {
-    console.log( numero,"este numero es impar")
 
-}
+var boton = document.getElementById("botonPrueba");
+
+boton.onclick = function otra() {
+    console.log("hola mundo")
+    for (let numero = 0; numero <= 20; numero++) {
+
+        if (numero % 2 === 0) {
+            console.log(numero, "este numero es par")
+        }
+        if (numero % 2 !== 0) {
+            console.log(numero, "este numero es impar")
+
+        }
 
 
+    }
 }
